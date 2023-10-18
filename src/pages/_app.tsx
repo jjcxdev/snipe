5a0886070db0607f884e26c7576b10c1ccfbfba2
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-10-18 11:14:01 -0400

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 
 export default MyApp;
